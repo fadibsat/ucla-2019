aae5847b79a98ce62bb5b80a01e2bf2ef2820ac1
Fadi <fadibsat@gmail.com>
2019-07-26 11:20:47 -0700

    third commit

---
+++ vscode/conversation.py
@@ -0,0 +1,64 @@
+name= input("what's your name?")
+print('hello ',name," my name is computer, how's your day been?")
+a=input()
+goodDay=['good','sick','ok','cool','nice','fun','great','well','very good','very well','acceptable','all right', 'fine','very nice','okay','satisfactory','so-so','very well','satisfying','gratisfiying','suitable','fitting']
+badDay=['rough','hard','tough','hell of a day','the worst day ever','the worst','bad hair day','bummer day','terrible day','crappy day','shitty day','fucked up day','very complicated','really bad','really long day','really long day','kind of a day',
+'kind of a day','rough one','bad old days','rough afternoon','less fortunate day','not good','worse days','very bad ','wrong night','fucking day','motherfucking day',
+'been bad',"it's been bad","really tough",'in a really bad mood','really bad mood','very difficult','not so perfect', 'down day','really rough','very trying','really hard'
+,'slow day','bad days','very hard','real bad','not a great day','the worst ','worst day ever','not the best','not great','most horrible day','your worst day','one of those days'
+,'very strange',
+'''
+wrong 
+unlucky 
+off 
+awful 
+difficult 
+long 
+good 
+lousy
+sad
+monday
+stressful
+horrible 
+misfortune
+distressing 
+junky
+fatiguing 
+rough 
+saddest
+full
+miserable
+emotional
+strenuous
+rough  
+not-so-perfect  
+dire  
+heavy  
+complicated  
+negative
+bad 
+shit 
+dreadful
+ghastly 
+hideous 
+trying
+rotten 
+busy 
+shitty 
+evil
+crap
+disheartening
+trashy
+personal
+darkest 
+bad name 
+disrepute
+reflects
+badly 
+negative''']
+if a in goodDay:
+    print("cool see you later ",name,".")
+if a in badDay:
+   print("too bad for you,",name,", my day's been great.")    
+
+    
---
+++ vscode/variablesPractice.py
@@ -0,0 +1,10 @@
+x = input("input x: ")
+y = input("input y: ")
+
+a=x
+x=y
+y=a
+
+print("I swapped x and y, look:")
+print("x is now ",x)
+print("y is now ",y)
